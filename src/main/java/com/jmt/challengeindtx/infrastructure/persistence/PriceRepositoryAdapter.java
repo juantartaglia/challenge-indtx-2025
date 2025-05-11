4a2731f4016266224432740fc9847875e22a0a13
Juan Manuel Tartaglia <juan.tartaglia@naranjax.com>
2025-05-11 20:22:02 +0100

    feat: implements getPriceByProductIdAndDateAndBrand case

--- src/main/java/com/jmt/challengeindtx/infrastructure/persistence/PriceRepositoryAdapter.java
+++ src/main/java/com/jmt/challengeindtx/infrastructure/persistence/PriceRepositoryAdapter.java
@@ -2,10 +2,15 @@ package com.jmt.challengeindtx.infrastructure.persistence;
 
 import com.jmt.challengeindtx.domain.model.Price;
 import com.jmt.challengeindtx.domain.port.out.PriceRepository;
+import com.jmt.challengeindtx.infrastructure.persistence.entity.PriceEntity;
+import com.jmt.challengeindtx.infrastructure.persistence.mapper.PriceMapper;
+import com.jmt.challengeindtx.infrastructure.persistence.repository.PriceH2Repository;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /**
  * Adapter class for PriceRepository.
@@ -15,8 +20,19 @@ import java.util.List;
 @Component
 public class PriceRepositoryAdapter implements PriceRepository {
 
+    private final PriceH2Repository repository;
+    private final PriceMapper priceMapper;
+
+    public PriceRepositoryAdapter(PriceH2Repository repository, PriceMapper priceMapper) {
+        this.repository = repository;
+        this.priceMapper = priceMapper;
+    }
+
     @Override
     public List<Price> getPriceByProductIdAndDateAndBrand(Long productId, LocalDateTime applicationDate, long brandId) {
-        return List.of();
+
+        return repository.findByProductIdAndBrandIdAndDate(productId, brandId, applicationDate)
+                .stream().map(priceMapper::toDomain)
+                .collect(Collectors.toList());
     }
 }
